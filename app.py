d1c0f7a3154d8580129bfc8b12a62a94161d95d5
Ata Atasoy <h.ataatasoy@gmail.com>
2020-01-08 00:43:47 +0300

    Converted it to a table that I log my CS related studies in

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+class Subject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
@@ -17,46 +15,44 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
 
 @app.route('/', methods=['POST', 'GET'])
-
 def index():
     if request.method == 'POST':
-        task_content = request.form['content']
-        new_Task = Todo(content=task_content)
+        subject_content = request.form['content']
+        new_subject = Subject(content=subject_content)
 
         try:
-            db.session.add(new_Task)
+            db.session.add(new_subject)
             db.session.commit()
             return redirect('/')
         except:
             return 'Could not add task'
 
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+        subjects = Subject.query.order_by(Subject.date_created).all()
+        return render_template('index.html', subjects=subjects)
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
+    subject_to_delete = Subject.query.get_or_404(id)
     try:
-        db.session.delete(task_to_delete)
-        db.commit()
+        db.session.delete(subject_to_delete)
+        db.session.commit()
         return redirect('/')
     except:
         return 'Cannot delete that task'
 
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    subject = Subject.query.get_or_404(id)
     if request.method == 'POST':
-        task.content = request.form['content']
-
+        subject.content = request.form['content']
         try:
             db.session.commit()
             return redirect('/')
         except:
-            return 'Could not update the task'
+            return 'Could not update the subject'
     else:
-        return render_template('update.html', task = task)
+        return render_template('update.html', subject = subject)
 
 if __name__ == "__main__":
     app.run(debug=True)
